13ed5872dc7af5277794f6f1b899d2916346179d
Harsh kumar joshi <joshihk99@gmail.com>
2018-10-12 16:47:10 +0530

    Update database.java

--- database.java
+++ database.java
@@ -1,4 +1,3 @@
-package com.example.lenovo.squlite_data;
 
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
@@ -6,9 +5,7 @@ import android.widget.EditText;
 
 import static android.content.Context.MODE_PRIVATE;
 
-/**
- * Created by lenovo on 11-05-2018.
- */
+
 
 public class database {
 
@@ -20,9 +17,9 @@ public class database {
         String n = name.getText().toString();
         String m = mobile.getText().toString();
         String c = city.getText().toString();
-        db = database.this.openOrCreateDatabase("harsh.db", MODE_PRIVATE, null);
-        db.execSQL("create table if not exists hj(name varchar,mobile varchar,city varchar);");
-        db.execSQL("insert into hj (name,mobile,city) values('" + n + "','" + m + "','" + c + "');");
+        db = database.this.openOrCreateDatabase("abc.db", MODE_PRIVATE, null);
+        db.execSQL("create table if not exists xyz(name varchar,mobile varchar,city varchar);");
+        db.execSQL("insert into xyz (name,mobile,city) values('" + n + "','" + m + "','" + c + "');");
         db.close();
         Intent intent = new Intent(database.this, display.class);
 
